e5dc5152f8766f495ae9aa3707cd27dfaa06f4f1
WagnerZMendes <51683073+WagnerZMendes@users.noreply.github.com>
2020-05-28 22:14:25 -0300

    Add files via upload

---
+++ Atividade.js
@@ -0,0 +1,50 @@
+var receitaFavorita = {
+    titulo : "Chocolate",
+    porcoes : 2,
+    ingredintes : "\nCacau, \nLeite, \nManteiga",
+}
+console.log (receitaFavorita.titulo,
+            receitaFavorita.porcoes,
+            receitaFavorita.ingredintes);
+
+let livros= [
+    {
+    titulo: "Atletico Ganhou",
+    autor: "CAM",
+    livroLido: True,
+    },
+
+    {
+    titulo:"Cruzeiro Perdeu",
+    autor: "CRU",
+    livroLido: False,
+    },
+
+    {
+    titulo:"Atletico o Melhor",
+    autor: "CAM",
+    livroLido: True,
+    },
+
+    {
+    titulo:"Cruzeiro o Pior",
+    autor: "CAM",
+    livroLido: False,
+    },
+]
+
+for(i = 0, i < livros.length; i++) {
+    console.log(livros.titulo);
+    console.log(livros.autor);
+    if(livros.livroLido = True){
+        console.log("Você já leu este livro"+titulo,+autor)
+    }else{
+        return "Você não leu este livro";
+    }
+}
+
+    
+
+        
+
+
